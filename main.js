197a39bcc42584732486a5cecf5e689bf395454b
Hani <hani.lebguiret1@gmail.com>
2021-12-29 12:35:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+for(let i = 0; i < projets.length; i++)
+
+$('#cards').append(`
+    <div id="card" style="background-image: url(${projets[i].bgImg});">
+        <div id="desc-card">
+            <p>${projets[i].description}</p>
+
+            <a class="btnMore" href="${projets[i].link}">
+                Voir le site
+                <i class="fas fa-angle-right"></i>
+            </a>
+        </div>
+    </div>
+`);
---
+++ projets.js
@@ -0,0 +1,32 @@
+const projets = [
+    {
+        description: 'lorem zoefozefio',
+        bgImg: "assets/tezos-WN5_7UBc7cw-unsplash.jpg",
+        link: "#"
+    },
+    {
+        description: 'lorem zoefozefio',
+        bgImg: "assets/tezos-WN5_7UBc7cw-unsplash.jpg",
+        link: "#"
+    },
+    {
+        description: 'lorem zoefozefio',
+        bgImg: "assets/tezos-WN5_7UBc7cw-unsplash.jpg",
+        link: "#"
+    },
+    {
+        description: 'lorem zoefozefio',
+        bgImg: "assets/tezos-WN5_7UBc7cw-unsplash.jpg",
+        link: "#"
+    },
+    {
+        description: 'lorem zoefozefio',
+        bgImg: "assets/tezos-WN5_7UBc7cw-unsplash.jpg",
+        link: "#"
+    },
+    {
+        description: 'Site ajout de produits via un formulaire en HTML/JS',
+        bgImg: "assets/tezos-WN5_7UBc7cw-unsplash.jpg",
+        link: "https://hanileb.github.io/ajout-articles/"
+    }
+];
