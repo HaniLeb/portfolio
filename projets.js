289c9a59e75f81ae25f2e9a5f6ff737a64ac05ee
Hani <hani.lebguiret1@gmail.com>
2021-12-29 14:32:50 +0100

    add cards

--- projets.js
+++ projets.js
@@ -10,23 +10,38 @@ const projets = [
         link: "https://hanileb.github.io/memory-game/"
     },
     {
-        description: 'Site de sport avec un formulaire pour se créer un profil, réaliser en HTML/CSS/jQuery',
+        description: 'Site de sport avec un formulaire pour se créer un profil, réaliser en HTML/CSS/jQuery/Bootstrap.',
         bgImg: "assets/site-sport.png",
         link: "https://hanileb.github.io/site-sport/"
     },
     {
-        description: 'Site de voyage, réaliser en HTML/CSS',
+        description: 'Site de voyage, réaliser en HTML/CSS.',
         bgImg: "assets/site-voyage.png",
         link: "https://hanileb.github.io/Site-voyage/"
     },
     {
-        description: 'Site vitrine, présentation d\'une ville, réaliser en HTML/CSS/Bootstrap',
+        description: 'Site vitrine, présentation d\'une ville, réaliser en HTML/CSS/Bootstrap.',
         bgImg: "assets/site-ville.png",
         link: "https://hanileb.github.io/Site-ville/"
     },
     {
-        description: 'Site pour créer des produits via un formulaire en HTML/jQuery',
+        description: 'Site pour créer des produits via un formulaire en HTML/jQuery/Bootstrap.',
         bgImg: "assets/site-articles.png",
         link: "https://hanileb.github.io/ajout-articles/"
+    },
+    {
+        description: 'Reproduction du site Le Monde, en HTML/CSS.',
+        bgImg: "assets/LeMonde.png",
+        link: "https://hanileb.github.io/LeMonde/"
+    },
+    {
+        description: 'Site vitrine responsive en HTML/CSS/Bootstrap.',
+        bgImg: "assets/OBriens.png",
+        link: "https://hanileb.github.io/OBriens/"
+    },
+    {
+        description: 'Site réaliser en HTML/CSS.',
+        bgImg: "assets/Chanel.png",
+        link: "https://hanileb.github.io/Chanel/"
     }
 ];
