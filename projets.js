d210152a6be21a55b8232cf985bfc5f2b68589a1
Hani <hani.lebguiret1@gmail.com>
2022-07-01 14:57:20 +0200

    change links

--- projets.js
+++ projets.js
@@ -1,57 +1,57 @@
 const projets = [
-    {
-        description: 'Site pour consulter les différents championnats d\'europe.',
-        bgImg: "assets/Foot.png",
-        link: "https://hanileb.github.io/100-FOOT/"
-    },
-    {
-        description: 'Site pour consulter les différents films et séries de différentes plateformes.',
-        bgImg: "assets/Cine.png",
-        link: "https://hanileb.github.io/Cin-/"
-    },
-    {
-        description: 'Jeux de dés en HTML/Js',
-        bgImg: "assets/dice-game.png",
-        link: "https://hanileb.github.io/dice-game/"
-    },
-    {
-        description: 'Jeux de mémorisation de carte Marvel, en HTML/CSS/Js',
-        bgImg: "assets/memory-game.png",
-        link: "https://hanileb.github.io/memory-game/"
-    },
-    {
-        description: 'Site de sport avec un formulaire pour se créer un profil, réalisé en HTML/CSS/jQuery/Bootstrap.',
-        bgImg: "assets/site-sport.png",
-        link: "https://hanileb.github.io/site-sport/"
-    },
-    {
-        description: 'Site de voyage, réalisé en HTML/CSS.',
-        bgImg: "assets/site-voyage.png",
-        link: "https://hanileb.github.io/Site-voyage/"
-    },
-    {
-        description: 'Site vitrine, présentation d\'une ville, réalisé en HTML/CSS/Bootstrap.',
-        bgImg: "assets/site-ville.png",
-        link: "https://hanileb.github.io/Site-ville/"
-    },
-    {
-        description: 'Site pour créer des produits via un formulaire en HTML/jQuery/Bootstrap.',
-        bgImg: "assets/site-articles.png",
-        link: "https://hanileb.github.io/ajout-articles/"
-    },
-    {
-        description: 'Reproduction du site Le Monde, en HTML/CSS.',
-        bgImg: "assets/LeMonde.png",
-        link: "https://hanileb.github.io/LeMonde/"
-    },
-    {
-        description: 'Site vitrine responsive en HTML/CSS/Bootstrap.',
-        bgImg: "assets/OBriens.png",
-        link: "https://hanileb.github.io/OBriens/"
-    },
-    {
-        description: 'Site réalisé en HTML/CSS.',
-        bgImg: "assets/Chanel.png",
-        link: "https://hanileb.github.io/Chanel/"
-    }
+  {
+    description: "Site pour consulter les différents championnats d'europe.",
+    bgImg: "assets/Foot.png",
+    link: "https://hanileb.github.io/Football/",
+  },
+  {
+    description: "Site pour consulter les différents films et séries de différentes plateformes.",
+    bgImg: "assets/Cine.png",
+    link: "https://hanileb.github.io/Cinema/",
+  },
+  {
+    description: "Jeux de dés en HTML/Js",
+    bgImg: "assets/dice-game.png",
+    link: "https://hanileb.github.io/dice-game/",
+  },
+  {
+    description: "Jeux de mémorisation de carte Marvel, en HTML/CSS/Js",
+    bgImg: "assets/memory-game.png",
+    link: "https://hanileb.github.io/memory-game/",
+  },
+  {
+    description: "Site de sport avec un formulaire pour se créer un profil, réalisé en HTML/CSS/jQuery/Bootstrap.",
+    bgImg: "assets/site-sport.png",
+    link: "https://hanileb.github.io/site-sport/",
+  },
+  {
+    description: "Site de voyage, réalisé en HTML/CSS.",
+    bgImg: "assets/site-voyage.png",
+    link: "https://hanileb.github.io/Site-voyage/",
+  },
+  {
+    description: "Site vitrine, présentation d'une ville, réalisé en HTML/CSS/Bootstrap.",
+    bgImg: "assets/site-ville.png",
+    link: "https://hanileb.github.io/Site-ville/",
+  },
+  {
+    description: "Site pour créer des produits via un formulaire en HTML/jQuery/Bootstrap.",
+    bgImg: "assets/site-articles.png",
+    link: "https://hanileb.github.io/ajout-articles/",
+  },
+  {
+    description: "Reproduction du site Le Monde, en HTML/CSS.",
+    bgImg: "assets/LeMonde.png",
+    link: "https://hanileb.github.io/LeMonde/",
+  },
+  {
+    description: "Site vitrine responsive en HTML/CSS/Bootstrap.",
+    bgImg: "assets/OBriens.png",
+    link: "https://hanileb.github.io/OBriens/",
+  },
+  {
+    description: "Site réalisé en HTML/CSS.",
+    bgImg: "assets/Chanel.png",
+    link: "https://hanileb.github.io/Chanel/",
+  },
 ];
