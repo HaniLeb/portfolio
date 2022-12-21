e2fa457eb68388dddfbc10117e01c9fbbe20a7b2
Hani <hani.lebguiret1@gmail.com>
2022-12-21 15:44:58 +0100

    corrections des titres et ajout de maquette wordpress

--- projets.js
+++ projets.js
@@ -1,57 +1,62 @@
 const projets = [
   {
-    description: "Site pour consulter les différents championnats d'europe.",
+    description: "Site de football, qui permet de voir le classement et les matchs de certains championnats d'Europe, avec HTML/CSS/JavaScript",
     bgImg: "assets/Foot.png",
     link: "https://hanileb.github.io/Football/",
   },
   {
-    description: "Site pour consulter les films et séries de différentes plateformes.",
+    description: "Site pour consulter les films et séries de différentes plateformes, avec HTML/CSS/JavaScript",
     bgImg: "assets/Cine.png",
     link: "https://hanileb.github.io/Cinema/",
   },
   {
-    description: "Jeux de dés en HTML/Js",
+    description: "Jeux de dés, avec HTML/JavaScript",
     bgImg: "assets/dice-game.png",
     link: "https://hanileb.github.io/dice-game/",
   },
   {
-    description: "Jeux de mémorisation de carte Marvel, en HTML/CSS/Js",
+    description: "Jeux de mémorisation de carte Marvel, avec HTML/CSS/JavaScript",
     bgImg: "assets/memory-game.png",
     link: "https://hanileb.github.io/memory-game/",
   },
   {
-    description: "Site de sport avec un formulaire pour se créer un profil, réalisé en HTML/CSS/jQuery/Bootstrap.",
+    description: "Site d'une salle de sport, avec un formulaire d'inscription, réalisé avec HTML/CSS/jQuery/Bootstrap.",
     bgImg: "assets/site-sport.png",
     link: "https://hanileb.github.io/site-sport/",
   },
   {
-    description: "Site de voyage, réalisé en HTML/CSS.",
+    description: "Maquette sur les voyages, réalisée avec HTML/CSS.",
     bgImg: "assets/site-voyage.png",
     link: "https://hanileb.github.io/Site-voyage/",
   },
   {
-    description: "Site vitrine, présentation d'une ville, réalisé en HTML/CSS/Bootstrap.",
+    description: "Maquette qui présente une ville, réalisée avec HTML/CSS/Bootstrap.",
     bgImg: "assets/site-ville.png",
     link: "https://hanileb.github.io/Site-ville/",
   },
   {
-    description: "Site pour créer des produits via un formulaire en HTML/jQuery/Bootstrap.",
+    description: "Site qui permet de créer des cartes de produits via un formulaire avec HTML/jQuery/Bootstrap.",
     bgImg: "assets/site-articles.png",
     link: "https://hanileb.github.io/ajout-articles/",
   },
   {
-    description: "Reproduction du site Le Monde, en HTML/CSS.",
+    description: "Maquette du site Le Monde avec HTML/CSS.",
     bgImg: "assets/LeMonde.png",
     link: "https://hanileb.github.io/LeMonde/",
   },
   {
-    description: "Site vitrine responsive en HTML/CSS/Bootstrap.",
+    description: "Maquette responsive avec HTML/CSS/Bootstrap.",
     bgImg: "assets/OBriens.png",
     link: "https://hanileb.github.io/OBriens/",
   },
   {
-    description: "Site réalisé en HTML/CSS.",
+    description: "Maquette réalisé avec HTML/CSS.",
     bgImg: "assets/Chanel.png",
     link: "https://hanileb.github.io/Chanel/",
+  },
+  {
+    description: "Maquette réalisé avec Wordpress",
+    bgImg: "assets/radio-jazz.png",
+    link: "http://lebguiret-hani.atwebpages.com/",
   },
 ];
